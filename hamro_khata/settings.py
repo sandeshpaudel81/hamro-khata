a5c2d6cd354cddb9a9d370ce1c1ed14a2db40e65
Sandesh Paudel <sandeshpaudel81@gmail.com>
2021-04-10 22:34:37 +0545

    static files conf for heroku

--- hamro_khata/settings.py
+++ hamro_khata/settings.py
@@ -122,6 +122,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
